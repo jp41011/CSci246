41042e767ec5220e90b9cac5ca58a9460477ff08
Juan <jp41011@gmail.com>
2016-11-20 21:30:36 -0800

    Boiler plate to start HW7 - Simulator for a cc-NUMA architecture with directory based cache coherence control (using write-invalidate protocol).

---
+++ hw7/hw7.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hw7.cpp
+// Author      : Juan Pedraza
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints
+	return 0;
+}
